15292ad9d2de2c43f0db008a62c72c168c089b02
yoshidayuri-1210 <totoro_sister@yahoo.co.jp>
2022-03-18 17:40:16 +0900

    TOPページのおすすCSSめ仕様変更

--- resources/views/items/top.blade.php
+++ resources/views/items/top.blade.php
@@ -10,6 +10,7 @@ use App\Item;
 
 <div class="top_search">
   <!--▼フリーワード検索ボックス-->
+  　　<p><img src="{{ asset('/storage/images/search_icon.png') }}">  まずは簡単検索！</p>
       <form method="get" action="{{ route('items.search') }}" class="search">
         <input type="text" name="keyword">
         <input type="submit" value="検索">
@@ -27,10 +28,11 @@ use App\Item;
 </h2>
 </div>
 
-<div class="user_category_items_content">
+<div class="top_recommend">
+  
   @forelse($recommend_category_items as $recommend_category_item)
-
-  <div class="top_category_item_img">
+  <a href="{{ route('items.show', $recommend_category_item) }}">
+  <div class="top_img">
     @if($recommend_category_item->image !== '')
       <img src="{{ asset('storage/' . $recommend_category_item->image) }}">
     @else
@@ -38,19 +40,22 @@ use App\Item;
     @endif
   </div>  
 
-  <div class="test">
+  <div class="top_text">
+  
+  <h3>{{ $recommend_category_item->title }}</h3>
   <ul>
   <li>法人名：{{ $recommend_category_item->company_name }}　店舗名：{{ $recommend_category_item->shop_name }}</li>
-  <li>{{ $recommend_category_item->title }}</li>
   <li><span>雇用形態：{{ Item::EMPLOYMENT[$recommend_category_item->employment] }}</span></li>
   <li><span>業種：{{ Item::TYPE[$recommend_category_item->type] }}</span></li>
-  <li><span>{{ $recommend_category_item->category->name }}</span></li>
-  <button type="button" onclick="location.href='{{ route('items.show', $recommend_category_item) }}'">この求人の詳細を見る</button>
+  <!--<li><span>{{ $recommend_category_item->category->name }}</span></li>-->
+  <!--<button type="button" onclick="location.href='{{ route('items.show', $recommend_category_item) }}'">この求人の詳細を見る</button>-->
   </ul>
+  </div>
   @empty こだわり条件の求人はありません
   @endforelse
+  </a>
 </div>
-</div>
+
 <!--▲こだわり条件の最新求人3つ-->
 
 
@@ -61,27 +66,47 @@ use App\Item;
 @else <h2>おすすめ新着求人</h2>
 @endif
 </div>
-<div class="user_area_items_content">
+
+<div class="top_recommend">
+  
   @forelse($recommend_area_items as $recommend_area_item)
+<a href="{{ route('items.show', $recommend_area_item) }}">
+  <div class="top_img">
+    @if($recommend_area_item->image !== '')
+      <img src="{{ asset('storage/' . $recommend_area_item->image) }}">
+    @else
+      <img src="{{ asset('storage/images/no_image.png') }}">
+    @endif
+  </div>  
+
+  <div class="top_text">
+  <h3>{{ $recommend_area_item->title }}</h3>
   <ul>
   <li>法人名：{{ $recommend_area_item->company_name }}　店舗名：{{ $recommend_area_item->shop_name }}</li>
-  <li>{{ $recommend_area_item->title }}</li>
+
   <li><span>雇用形態：{{ Item::EMPLOYMENT[$recommend_area_item->employment] }}</span></li>
   <li><span>業種：{{ Item::TYPE[$recommend_area_item->type] }}</span></li>
   <li><span>{{ $recommend_area_item->category->name }}</span></li>
-  <button type="button" onclick="location.href='{{ route('items.show', $recommend_area_item) }}'">この求人の詳細を見る</button>
   </ul>
+  </div>
   @empty 希望エリア条件の求人はありません
   @endforelse
-<!--▲希望エリアの最新求人3つ-->
+</a>
 </div>
+<!--▲希望エリアの最新求人3つ-->
 
+<div class="area_search">
+<div class="area_search_title">
   <h2>都道府県から探す</h2>
+</div>
+<div class="area_search_content">
   @foreach($area_ids as $area_id)
   <ul>
   <li><a href="{{ route('items.index', $area_id->id) }}">{{ $area_id->name }}</a></li>
   </ul>
   @endforeach
+</div>
+</div>
 
   <h2>こだわり条件から探す</h2>
   @foreach($categories as $category)
@@ -89,7 +114,8 @@ use App\Item;
     <li><a href="{{ route('items.category', $category) }}">{{ $category->name }}</a></li>
   </ul>
   @endforeach
-  
+
+
   <h2>業種から探す</h2>
   @foreach($types as $type)
   <ul>
