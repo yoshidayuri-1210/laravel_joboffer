6e6ea495beb1212631cb13359bdb52dce4d8df2b
yoshidayuri-1210 <totoro_sister@yahoo.co.jp>
2022-03-20 22:05:16 +0900

    TOPページ仕様変更（全体）

--- resources/views/items/top.blade.php
+++ resources/views/items/top.blade.php
@@ -8,15 +8,18 @@ use App\Item;
  
 @section('content')
 
+
 <div class="top_search">
   <!--▼フリーワード検索ボックス-->
-  　　<p><img src="{{ asset('/storage/images/search_icon.png') }}">  まずは簡単検索！</p>
+  　　<p><img src="{{ asset('/storage/images/search_icon.png') }}">  かんたん求人検索！/</p>
       <form method="get" action="{{ route('items.search') }}" class="search">
         <input type="text" name="keyword">
         <input type="submit" value="検索">
       </form>
   <!--▲フリーワード検索ボックス-->
 </div>
+
+<div class="container">
 
 <!--▼こだわり条件の最新求人3つ-->
 <div class="user_category_title">
@@ -29,7 +32,6 @@ use App\Item;
 </div>
 
 <div class="top_recommend">
-  
   @forelse($recommend_category_items as $recommend_category_item)
   <a href="{{ route('items.show', $recommend_category_item) }}">
   <div class="top_img">
@@ -41,21 +43,18 @@ use App\Item;
   </div>  
 
   <div class="top_text">
-  
-  <h3>{{ $recommend_category_item->title }}</h3>
+  <h3>{{ $recommend_category_item->company_name }} {{ $recommend_category_item->shop_name }}</h3>
   <ul>
-  <li>法人名：{{ $recommend_category_item->company_name }}　店舗名：{{ $recommend_category_item->shop_name }}</li>
-  <li><span>雇用形態：{{ Item::EMPLOYMENT[$recommend_category_item->employment] }}</span></li>
-  <li><span>業種：{{ Item::TYPE[$recommend_category_item->type] }}</span></li>
-  <!--<li><span>{{ $recommend_category_item->category->name }}</span></li>-->
-  <!--<button type="button" onclick="location.href='{{ route('items.show', $recommend_category_item) }}'">この求人の詳細を見る</button>-->
+  <li><span class="top_text_title">{{ $recommend_category_item->title }}</span></li>
+  <li><span>{{ Item::EMPLOYMENT[$recommend_category_item->employment] }}</span></li>
+  <li><span>{{ $recommend_category_item->category->name }}</span></li>
+  <li><span>{{ Item::TYPE[$recommend_category_item->type] }}</span></li>
   </ul>
   </div>
   @empty こだわり条件の求人はありません
   @endforelse
   </a>
 </div>
-
 <!--▲こだわり条件の最新求人3つ-->
 
 
@@ -68,7 +67,6 @@ use App\Item;
 </div>
 
 <div class="top_recommend">
-  
   @forelse($recommend_area_items as $recommend_area_item)
 <a href="{{ route('items.show', $recommend_area_item) }}">
   <div class="top_img">
@@ -78,15 +76,13 @@ use App\Item;
       <img src="{{ asset('storage/images/no_image.png') }}">
     @endif
   </div>  
-
   <div class="top_text">
-  <h3>{{ $recommend_area_item->title }}</h3>
+  <h3>{{ $recommend_area_item->company_name }} {{ $recommend_area_item->shop_name }}</h3>
   <ul>
-  <li>法人名：{{ $recommend_area_item->company_name }}　店舗名：{{ $recommend_area_item->shop_name }}</li>
-
-  <li><span>雇用形態：{{ Item::EMPLOYMENT[$recommend_area_item->employment] }}</span></li>
-  <li><span>業種：{{ Item::TYPE[$recommend_area_item->type] }}</span></li>
+  <li><span class="top_text_title">{{ $recommend_area_item->title }}</span></li>
+  <li><span>{{ Item::EMPLOYMENT[$recommend_area_item->employment] }}</span></li>
   <li><span>{{ $recommend_area_item->category->name }}</span></li>
+  <li><span>{{ Item::TYPE[$recommend_area_item->type] }}</span></li>
   </ul>
   </div>
   @empty 希望エリア条件の求人はありません
@@ -108,28 +104,44 @@ use App\Item;
 </div>
 </div>
 
+<div class="category_search">
+<div class="category_search_title">
   <h2>こだわり条件から探す</h2>
+</div>
+<div class="category_search_content">
   @foreach($categories as $category)
   <ul>
     <li><a href="{{ route('items.category', $category) }}">{{ $category->name }}</a></li>
   </ul>
   @endforeach
+</div>
+</div>
 
-
+<div class="type_search">
+<div class="type_search_title">
   <h2>業種から探す</h2>
+</div>
+<div class="type_search_content">  
   @foreach($types as $type)
   <ul>
     <li><a href="{{ route('items.type', array_keys($types, $type)) }}">{{ $type }}</a></li>
   </ul>
   @endforeach
+</div>
+</div>
 
+<div class="employment_search">
+<div class="employment_search_title">
   <h2>雇用形態から探す</h2>
+</div>
+<div class="employment_search_content">
    @foreach($employments as $employment)
   <ul>
     <li><a href="{{ route('items.employment', array_keys($employments, $employment)) }}">{{ $employment }}</a></li>
   </ul>
   @endforeach
-
+</div>
+</div>
  
-
+</div>
 @endsection
--- resources/views/likes/index.blade.php
+++ resources/views/likes/index.blade.php
@@ -7,16 +7,19 @@ use App\Item;
 @section('title', $title)
  
 @section('content')
-    <h1>{{ $title }}</h1>
-        @forelse($like_items as $like_item)
-            <p>{{ $like_item->company_name }} {{ $like_item->shop_name }}</p>
-            <p><a href="{{ route('items.show', $like_item) }}">{{ $like_item->title }}</p></a>
-            <span>雇用形態：{{ Item::EMPLOYMENT[$like_item->employment] }}</span>
-            <span>業種：{{ Item::TYPE[$like_item->type] }}</span>
-            <span>{{ $like_item->category->name }}</span></br>
-            <button type="button" onclick="location.href='{{ route('items.show', $like_item) }}'">この求人の詳細を見る</button>
-        @empty
-            <p>保存した求人はありません</p>
-        @endforelse
-
+    <h1>保存した求人一覧</h1>
+    <div class="container">
+    @forelse($like_items as $like_item)
+    <div class="items_content">
+        <p>{{ $like_item->company_name }} {{ $like_item->shop_name }}</p>
+        <p><a href="{{ route('items.show', $like_item) }}">{{ $like_item->title }}</p></a>
+        <span>雇用形態：{{ Item::EMPLOYMENT[$like_item->employment] }}</span>
+        <span>業種：{{ Item::TYPE[$like_item->type] }}</span>
+        <span>{{ $like_item->category->name }}</span></br>
+        <button type="button" onclick="location.href='{{ route('items.show', $like_item) }}'">この求人の詳細を見る</button>
+    </div>
+    @empty
+        <p>保存した求人はありません</p>
+    @endforelse
+    </div>
 @endsection
