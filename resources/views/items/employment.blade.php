f2f672545b480fb946dbfeb748a83515556aaebf
yoshidayuri-1210 <totoro_sister@yahoo.co.jp>
2022-03-21 23:45:25 +0900

    item_contentのCSS仕様変更と画像表示

--- resources/views/items/employment.blade.php
+++ resources/views/items/employment.blade.php
@@ -9,15 +9,43 @@ use App\Item;
 @section('content')
 
 <h1>{{ $employment }}の求人一覧</h1>
+<div class="container">
   @forelse($items as $item) 
-  <h3><a href="{{ route('items.show', $item) }}">{{ $item->title }}</a></h3>
+<div class="item_content">
+  <span class="item_content_title"><h2><a href="{{ route('items.show', $item) }}">{{ $item->title }}</a></h2></span>
+    <h3>{{ $item->company_name }} {{ $item->shop_name }}</h3>
+    <span class="item_content_employment">{{ Item::EMPLOYMENT[$item->employment] }}</span>
+    <span class="item_content_type">{{ Item::TYPE[$item->type] }}</span>
+		<div class="item_content_location">
+			<dl class="item_content_area_id">
+				<dt>勤務地</dt>
+				<dd>{{ $item->area->name }}</dd>
+			</dl>
+			<dl class="item_content_access">
+				<dt>アクセス</dt>
+				<dd>{{ $item->access }}</dd>
+			</dl>
+		</div>
+
+      <div class="item_content_img">
+          @if($item->image !== '')
+            <img src="{{ asset('storage/' . $item->image) }}">
+          @else
+            <img src="{{ asset('storage/images/no_image.png') }}">
+          @endif
+          <div class="item_content_img_text">
+            {!! nl2br(e($item->description)) !!}
+          </div>
+      </div>  
+
+
     <ul>
-    <li>法人名：{{ $item->company_name }}　店舗名：{{ $item->shop_name }}</br></li>
+    
     <li>ポイント　{{ $item->category->name }}</li>
     <li>給与：{{ $item->payment_min }}円 ~ {{ $item->payment_max }}円</li>
-    <li>雇用形態：{{ Item::EMPLOYMENT[$item->employment] }}</li>
     </ul>
+</div>
   @empty 求人がありません
   @endforelse
-    
+</div>    
 @endsection
--- resources/views/likes/index.blade.php
+++ resources/views/likes/index.blade.php
@@ -10,16 +10,38 @@ use App\Item;
     <h1>保存した求人一覧</h1>
     <div class="container">
     @forelse($like_items as $like_item)
-    <div class="items_content">
-        <p>{{ $like_item->company_name }} {{ $like_item->shop_name }}</p>
-        <p><a href="{{ route('items.show', $like_item) }}">{{ $like_item->title }}</p></a>
-        <span>雇用形態：{{ Item::EMPLOYMENT[$like_item->employment] }}</span>
-        <span>業種：{{ Item::TYPE[$like_item->type] }}</span>
+    <div class="item_content">
+        <span class="item_content_title"><h2><a href="{{ route('items.show', $like_item) }}">{{ $like_item->title }}</a></h2></span>
+        <h3>{{ $like_item->company_name }} {{ $like_item->shop_name }}</h3>
+        <span class="item_content_employment">{{ Item::EMPLOYMENT[$like_item->employment] }}</span>
+        <span class="item_content_type">{{ Item::TYPE[$like_item->type] }}</span>
+		<div class="item_content_location">
+			<dl class="item_content_area_id">
+				<dt>勤務地</dt>
+				<dd>{{ $like_item->area->name }}</dd>
+			</dl>
+			<dl class="item_content_access">
+				<dt>アクセス</dt>
+				<dd>{{ $like_item->access }}</dd>
+			</dl>
+		</div>
+        
+        <div class="item_content_img">
+            @if($like_item->image !== '')
+              <img src="{{ asset('storage/' . $like_item->image) }}">
+            @else
+              <img src="{{ asset('storage/images/no_image.png') }}">
+            @endif
+            <div class="item_content_img_text">
+                {!! nl2br(e($like_item->description)) !!}
+            </div>
+        </div>  
+
         <span>{{ $like_item->category->name }}</span></br>
         <button type="button" onclick="location.href='{{ route('items.show', $like_item) }}'">この求人の詳細を見る</button>
     </div>
     @empty
         <p>保存した求人はありません</p>
-    @endforelse
+@endforelse
     </div>
 @endsection
