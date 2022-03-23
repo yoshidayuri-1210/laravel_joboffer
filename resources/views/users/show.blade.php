4244fc74d0c0a8877fbf6dfdeb43f58fdae8d290
yoshidayuri-1210 <totoro_sister@yahoo.co.jp>
2022-03-24 00:41:32 +0900

    profile画面のCSS仕様変更

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,18 +7,39 @@
   <div class="profile">
 
   <div class="profile_title"><h1>{{ $title }}</h1></div>
-  <p>名前：{{ $user->name }}  さん</p>
-  <p>希望勤務地：@if(!empty($user->area->name)){{ $user->area->name }}@else希望勤務地設定はありません</p>@endif
-  <p>こだわり条件：@if(!empty($user->category->name)){{ $user->category->name }}@elseこだわり条件の設定はありません</p>@endif
-  <p>プロフィール：</br>@if(!empty($user->profile)){!! nl2br(e($user->profile)) !!}@elseプロフィールの設定がありません</p>@endif
-  <p><a href="{{ route('profile.edit') }}">[編集]</a></p>
+
+  <div class="profile_table">
+            <table>
+                <tbody>
+                    <tr>
+                        <th>名前</th>
+                        <td>{{ $user->name }} さん</td>
+                    </tr>
+                    <tr>
+                        <th>希望勤務地</th>
+                        <td>@if(!empty($user->area->name)){{ $user->area->name }}@else希望勤務地設定はありません@endif</td>
+                    </tr>
+                    <tr>
+                        <th>こだわり条件</th>
+                        <td>@if(!empty($user->category->name)){{ $user->category->name }}@elseこだわり条件の設定はありません@endif</td>
+                    </tr>
+                    <tr>
+                      <th>プロフィール</th>
+                      <td>@if(!empty($user->profile)){!! nl2br(e($user->profile)) !!}@elseプロフィールの設定がありません@endif</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+  
+  
+  <div class="profile_edit_buttun"><a href="{{ route('profile.edit') }}">編集</a></div>
 
   <div class="profile_order_title"><h1>応募履歴</h1></div>
   @forelse($order_items as $order_item)
   <ul>
     <div class="ordering_list">
-      <li><a href="{{ route('items.show', $order_item) }}">法人名：{{ $order_item->company_name }} 店舗名：{{ $order_item->shop_name }}
-      (応募日:{{ $order_item->pivot->created_at }})</a></li>
+      <li><a href="{{ route('items.show', $order_item) }}">法人名：{{ $order_item->company_name }} 店舗名：{{ $order_item->shop_name }}</a>
+      (応募日:{{ $order_item->pivot->created_at }})</li>
     </div>
   </ul>
   @empty
